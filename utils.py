e42044867556c8b962f282bc565912c4b7644472
JJJ <123405186@qq.com>
2022-04-08 13:35:38 +0000

    alexnet

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import torchvision
 from torch.utils import data
 from torchvision import transforms
@@ -7,6 +8,7 @@ from IPython import display
 from matplotlib import pyplot as plt
 import time
 import numpy as np
+from multiprocessing import cpu_count
 
 def try_gpu(i=0):
     """Return gpu(i) if exists, otherwise return cpu().
@@ -16,9 +18,10 @@ def try_gpu(i=0):
         return torch.device(f'cuda:{i}')
     return torch.device('cpu')
 
-def load_data_fashion_mnist(batch_size, num_workers=4, resize=None):  #@save
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
     """下载Fashion-MNIST数据集，然后将其加载到内存中"""
     trans = [transforms.ToTensor()]
+    num_workers = cpu_count()
     if resize:
         trans.insert(0, transforms.Resize(resize))
     trans = transforms.Compose(trans)
@@ -215,3 +218,64 @@ class Timer:
     def cumsum(self):
         """Return the accumulated time."""
         return np.array(self.times).cumsum().tolist()
+
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    '''使用GPU计算模型在数据集上的精度。'''
+    if isinstance(net, nn.Module):
+        net.eval()
+        if not device:
+            # device没有告诉，就拿网络的device
+            device = next(iter(net.parameters())).device
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        if isinstance(X, list):
+            X = [x.to(device) for x in X]
+        else:
+            X = X.to(device)
+        y = y.to(device)
+        # accuracy为正确的个数
+        metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+
+def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+    '''Train a model with GPU (defined in Chapter 6).'''
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+    net.apply(init_weights)
+    print('training on', device)
+    net.to(device)
+    optimiezer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = Animator2(xlabel='epoch', xlim=[1, num_epochs],
+                              legend=['train loss', 'train acc', 'test acc'])
+    timer, num_batches = Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        metric = Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimiezer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimiezer.step()
+            metric.add(l * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (train_l, train_acc, None) )
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f'loss, {train_l:.3f}, train_acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+          f'on {str(device)}')
+
